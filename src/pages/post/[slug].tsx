f74cd8ae246f09f8b9f31cfbda7aba00d6b404c3
ChristianDaniel <christian_daniel04@hotmail.com>
2021-06-20 14:32:11 -0300

    implementando o botão exit-preview

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
 import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
 
@@ -32,9 +33,10 @@ interface Post {
 
 interface PostProps {
   post: Post;
+  preview: boolean;
 }
 
-export default function Post({ post }: PostProps): JSX.Element {
+export default function Post({ post, preview }: PostProps): JSX.Element {
   const totalWords = post.data.content.reduce((total, contentItem) => {
     // eslint-disable-next-line no-param-reassign
     total += contentItem.heading.split(' ').length;
@@ -101,6 +103,14 @@ export default function Post({ post }: PostProps): JSX.Element {
               </article>
             );
           })}
+
+          {preview && (
+            <aside>
+              <Link href="/api/exit-preview">
+                <a className={commonStyles.preview}>Sair do modo preview</a>
+              </Link>
+            </aside>
+          )}
         </div>
       </main>
     </>
@@ -127,10 +137,16 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps = async context => {
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData,
+}) => {
   const prismic = getPrismicClient();
-  const { slug } = context.params;
-  const response = await prismic.getByUID('posts', String(slug), {});
+  const { slug } = params;
+  const response = await prismic.getByUID('posts', String(slug), {
+    ref: previewData?.ref || null,
+  });
 
   const post = {
     uid: response.uid,
@@ -155,6 +171,7 @@ export const getStaticProps: GetStaticProps = async context => {
   return {
     props: {
       post,
+      preview,
     },
   };
 };

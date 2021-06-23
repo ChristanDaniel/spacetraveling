d17affa095162e37ddb4f8bb81630214a1b29f1f
ChristianDaniel <christian_daniel04@hotmail.com>
2021-06-23 13:44:02 -0300

    1 erro arrumado

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,17 +1,27 @@
-import { useEffect } from 'react';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { Component } from 'react';
 
-export default function Comments(): JSX.Element {
-  useEffect(() => {
+export default class Comments extends Component {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  componentDidMount() {
     const script = document.createElement('script');
     const anchor = document.getElementById('inject-comments-for-uterances');
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('crossorigin', 'anonymous');
+    script.setAttribute('async', 'true');
     script.setAttribute('repo', 'ChristanDaniel/spacetraveling');
     script.setAttribute('issue-term', 'pathname');
-    script.setAttribute('theme', 'github-dark');
-    script.setAttribute('async', 'async');
+    script.setAttribute('theme', 'dark-blue');
     anchor.appendChild(script);
-  }, []);
+  }
 
-  return <div id="inject-comments-for-uterances" />;
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  render() {
+    return (
+      <div
+        id="inject-comments-for-uterances"
+        style={{ marginBottom: '4rem' }}
+      />
+    );
+  }
 }
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -133,7 +133,7 @@ export default function Post({
               </div>
             )}
 
-            {navigation?.prevPost.length > 0 && (
+            {navigation?.nextPost.length > 0 && (
               <div>
                 <h3>{navigation.nextPost[0].data.title}</h3>
                 <Link href={`/post/${navigation.nextPost[0].uid}`}>
@@ -202,7 +202,7 @@ export const getStaticProps: GetStaticProps = async ({
     {
       pageSize: 1,
       after: response.id,
-      orderings: '[document.last_publication_date]',
+      orderings: '[document.last_publication_date desc]',
     }
   );
 
